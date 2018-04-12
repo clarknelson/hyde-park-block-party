e5c6d8dd7a0a59bb38745748b9cd1a93193a794c
Clark Nelson <mail@clarknelson.com>
2018-04-12 15:08:25 -0500

    update to functionality and styles

--- js/main.js
+++ js/main.js
@@ -110,6 +110,7 @@ $(document).ready(function(){
       }
     }
 
+
     function trigger_switch(){
       flicker(colors[(color_index%colors.length)], 0);
       color_index=color_index+1;
@@ -133,8 +134,69 @@ $(document).ready(function(){
     });
   }
 
+  function effect4(){
+    var speed = 100;
+    var $el = $('.graphic img');
+    var all_colors = 'green orange purple cyan yellow';
+    var colors = ['green', 'orange', 'purple', 'cyan', 'yellow', 'orange', 'cyan'];
+    // var color_index = 0;
+
+    var color_index = 0, index = 0;
+    var timeout_interval = setTimeout(tick, 1000);
+
+    function tick(){
+      clearTimeout(timeout_interval);
+      index=index+1;
+      var color = colors[color_index%colors.length];
+
+      if(index%4 === 0){
+        index=0;
+        color_index=color_index+1;
+      }
+
+      $('.top, .bottom').removeClass('left right');
+      if(index%4 === 1){
+        $('.top').addClass('right');
+        $('.bottom').addClass('left');
+      }
+      if(index%4 === 2){
+        $('.top').addClass('left');
+        $('.bottom').addClass('right');
+      }
+      if(index%4 === 3){
+        $('.top').addClass('right');
+        $('.bottom').addClass('left');
+      }
+
+      $('.bottom').removeClass(all_colors);
+      $('.bottom').addClass(color);
+
+      $el.attr('src', 'img/'+color+'/'+color+index+'.jpg');
+
+      timeout_interval = setTimeout(tick, 1000);
+    }
+
+    var totalDistance = 0;
+    var lastSeenAt = {x: null, y: null};
+
+    $(window).mousemove(function(event) {
+      if(lastSeenAt.x) {
+        totalDistance += Math.sqrt(Math.pow(lastSeenAt.y - event.clientY, 2) + Math.pow(lastSeenAt.x - event.clientX, 2));
+        $('span').text('So far your mouse ran this many pixels:   ' + Math.round(totalDistance));
+
+        if(totalDistance > 125){
+          tick();
+          totalDistance = 0;
+        }
+      }
+      lastSeenAt.x = event.clientX;
+      lastSeenAt.y = event.clientY;
+    });
+  }
+
   // effect1();
   // effect2();
-  effect3();
+  // effect3();
+  effect4();
 
 });
