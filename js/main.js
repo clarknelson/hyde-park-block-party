0d5b35e270b415c7f6411275c2e00c73cd06fc81
Clark Nelson <mail@clarknelson.com>
2018-06-29 17:45:44 -0500

    finish uno version

--- js/main.js
+++ js/main.js
@@ -1,140 +1,31 @@
 $(document).ready(function(){
-
-  function effect1(){
-    var delta = 200;
-    var $el = $('.graphic img');
-    var all_colors = 'green orange purple cyan yellow';
-    var colors = ['green', 'orange', 'purple', 'cyan', 'yellow', 'orange', 'cyan'];
-    var color_index = 0;
-
-    function tick(index){
-      var color = colors[color_index%colors.length];
-      $el.attr('src', 'img/'+color+'/'+color+(index%4)+'.jpg');
-      index++;
-      $('.top').removeClass('left right');
-      $('.bottom').removeClass('left right');
-      if(index%4 === 0){
-        color_index=color_index+1;
-      }
-      if(index%4 === 1){
-        $('.top').addClass('left');
-        $('.bottom').addClass('right');
-      }
-      if(index%4 === 2){
-        $('.top').addClass('right');
-        $('.bottom').addClass('left');
-      }
-      $('.bottom').removeClass(all_colors);
-      $('.bottom').addClass(color);
-      setTimeout(function(){
-        tick(index);
-      }, (delta + (delta * Math.random())));
+  function calculate(){
+    var right_width = $('#viewport .right').innerWidth();
+    var right_height = $('#viewport .right').innerHeight();
+    var ratio = right_width/right_height;
+    console.log(ratio);
+    if(ratio < 1){
+      $('body').addClass('set-text-to-width');
+      $('body').removeClass('set-text-to-height');
+    } else {
+      $('body').addClass('set-text-to-height');
+      $('body').removeClass('set-text-to-width');
     }
-    tick(0);
   }
+  calculate();
+  $(window).resize(_.throttle(calculate,100));
 
-
-
-
-
-
-
-  function effect2(){
-
-    var speed = 100;
-    var $el = $('.graphic img');
-    var all_colors = 'green orange purple cyan yellow';
-    var colors = ['green', 'orange', 'purple', 'cyan', 'yellow', 'orange', 'cyan'];
-    var color_index = 0;
-
-    function flicker(color, index){
-      var i = index+1;
-      $('.bottom').removeClass(all_colors);
-      $('.top').removeClass('left right');
-      $('.bottom').removeClass('left right');
-      $('.bottom').addClass(color);
-      if(i===1){
-        $('.top').addClass('right');
-        $('.bottom').addClass('left');
-      }
-      if(i===2){
-        $('.top').addClass('left');
-        $('.bottom').addClass('right');
-      }
-      if(i<4){
-        $el.attr('src', 'img/'+color+'/'+color+i+'.jpg');
-        setTimeout(function(){
-          flicker(color, i);
-        }, (speed + (speed * Math.random())) );
-      } else {
-        i = 0;
-        $el.attr('src', 'img/'+color+'/'+color+i+'.jpg');
-      }
+  var hasInvert = false;
+  setInterval(function(){
+    $('body').toggleClass('inverted');
+    if(hasInvert){
+      hasInvert = false;
+      $('body').removeClass('inverted');
+      $('#viewport .right .logo-grid img').attr('src', 'img/logo-grid-right-black.png');
+    } else {
+      hasInvert = true;
+      $('body').addClass('inverted');
+      $('#viewport .right .logo-grid img').attr('src', 'img/logo-grid-right-white.png');
     }
-
-    setInterval(function(){
-      flicker(colors[(color_index%colors.length)], 0);
-      color_index=color_index+1;
-    }, 1500);
-  }
-
-  function effect3(){
-    var speed = 100;
-    var $el = $('.graphic img');
-    var all_colors = 'green orange purple cyan yellow';
-    var colors = ['green', 'orange', 'purple', 'cyan', 'yellow', 'orange', 'cyan'];
-    var color_index = 0;
-
-    function flicker(color, index){
-      var i = index+1;
-      $('.bottom').removeClass(all_colors);
-      $('.top').removeClass('left right');
-      $('.bottom').removeClass('left right');
-      $('.bottom').addClass(color);
-      if(i===1){
-        $('.top').addClass('right');
-        $('.bottom').addClass('left');
-      }
-      if(i===2){
-        $('.top').addClass('left');
-        $('.bottom').addClass('right');
-      }
-      if(i<4){
-        $el.attr('src', 'img/'+color+'/'+color+i+'.jpg');
-        setTimeout(function(){
-          flicker(color, i);
-        }, (speed + (speed * Math.random())) );
-      } else {
-        i = 0;
-        $el.attr('src', 'img/'+color+'/'+color+i+'.jpg');
-      }
-    }
-
-    function trigger_switch(){
-      flicker(colors[(color_index%colors.length)], 0);
-      color_index=color_index+1;
-    }
-
-    var totalDistance = 0;
-    var lastSeenAt = {x: null, y: null};
-
-    $(window).mousemove(function(event) {
-      if(lastSeenAt.x) {
-        totalDistance += Math.sqrt(Math.pow(lastSeenAt.y - event.clientY, 2) + Math.pow(lastSeenAt.x - event.clientX, 2));
-        $('span').text('So far your mouse ran this many pixels:   ' + Math.round(totalDistance));
-
-        if(totalDistance > 250){
-          trigger_switch();
-          totalDistance = 0;
-        }
-      }
-      lastSeenAt.x = event.clientX;
-      lastSeenAt.y = event.clientY;
-    });
-  }
-
-  // effect1();
-  // effect2();
-  effect3();
-
+  }, 800);
 });
